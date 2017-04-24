35813aefb8ba4a0afc58b0f3a54b832e5dc72e80
Kazuhiro Kubota <k2.wanko@gmail.com>
2017-04-25 00:14:46 +0900

    Fix template path

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
+const path = require('path')
 const functions = require('firebase-functions')
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase)
 
-const htmlTemplate = fs.readFileSync('./dist/index.html', 'utf-8')
+const htmlTemplate = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8')
 
 const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
 let bundleRenderer = createBundleRenderer(require('./dist/vue-ssr-bundle.json'), {
